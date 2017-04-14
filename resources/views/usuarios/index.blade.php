4ec21329f6ec7695306cf9b4743c30c16ea20b46
Kevin Rivera <kevinrev26@gmail.com>
2017-04-14 12:42:12 -0600

    Comenzando navegacion

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -6,7 +6,22 @@
     <h1>{{ $user->name }}</h1>
     <h2>{{ $user->email }}</h2>
     <h3>{{ $user->role->nombre_rol }}</h3>
-    
+    {{-- @if ($user->role->menus->items)
+      @foreach ($item as $user->role->menus->items)
+        <p> {{$item->nombre_item}}, <a href="{{ $item->ruta }}"></a>  </p>
+      @endforeach
+    @endif --}}
+    @if (count($user->role->menus) > 0)
+      @foreach ($user->role->menus as $menu)
+        @foreach ($menu->items as $item)
+          <p>Item: {{ $item->nombre_item  }}, Enlace: <a href="{{ $item->ruta }}">{{ $item->ruta }}</a></p>
+        @endforeach
+      @endforeach
+    @else
+      <h4>No existe menú asociado.</h4>
+    @endif
+
+
     <hr />
   @endforeach
 
