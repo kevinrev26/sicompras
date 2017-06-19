b955ea927f4544f62531a1c6316c99fa017e7886
Luis PM <plpm90_nath@hotmail.com>
2017-06-19 13:22:06 -0600

    Finalizado

--- resources/views/ordenes/create.blade.php
+++ resources/views/ordenes/create.blade.php
@@ -10,27 +10,41 @@
 		<br/>
 	</div>
 
-  @else
-	  
+@else
     <div class="panel panel-default">
-        <div class="panel-heading center">
+        <div class="panel-heading">
           Agregar una nueva orden de compra. Fecha: {{ date('l F o') }}
         </div>
         <div class="panel-body">
           <form class="form-horizontal" method="post" action="{{ url('/purchaseorders') }}">
             {{ csrf_field() }}
             <input type="hidden" name="usuario" value="{{ Auth::user()->id }}"/>
-            <input type="hidden" name="oferta" value="{{ session('oferta') }}" />
+            <input type="hidden" name="oferta" value="{{ $oferta->id }}" />
             <div class="form-group">
-				<div class="col-sm-12 center">
-					<button type="submit" class="btn btn-success">Agregar Oferta</button>
-				</div>
-			</div>
+              <label class="control-label col-sm-2" for="proveedor">Proveedor: </label>
+              <span> {{$oferta->retail->name}} </span>
+            </div>
+            <div class="form-group">
+              <label class="control-label col-sm-2">Fecha de entrega: </label>
+              <input name="fecha-entrega" type="date" placeholder="dd/MM/YYYY" required />
+            </div>
+            {{-- <div class="form-group">
+              <ul>
+                @foreach ($oferta->bidding->solicitude->equipos as $equipo)
+                  <li>
+                    {{$equipo->nombre_equipo}}
+                  </li>
+                @endforeach
+              </ul>
+            </div> --}}
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Agregar Orden de compra</button>
+                </div>
+              </div>
           </form>
-		</div>
         </div>
     </div>
-	
   @endif
 
 @endsection
