698bce48ad2bd61a16e858c1d979b06056d95073
Kevin Rivera <kevinrev26@gmail.com>
2017-06-18 18:02:31 -0600

    Formulario de compras validado

--- resources/views/compras/create.blade.php
+++ resources/views/compras/create.blade.php
@@ -14,26 +14,26 @@
       </ul>
     </div>
   @endif
-  
+
   <form class="form-horizontal" method="POST" action="{{ url('/purchases') }}">
     {{ csrf_field() }}
     <div class="form-group">
       <label class="control-label col-sm-2" for="fecha">Fecha de compra:</label>
         <div class="col-sm-10">
-        <input type="date" class="form-control" id="fecha" name="fecha" placeholder="22/12/2017">
+        <input type="date" class="form-control" id="fecha" name="fecha" placeholder="22/12/2017" required>
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-2" for="orden">Número de Órden de compra</label>
         <div class="col-sm-10">
-        <input type="text" class="form-control" id="orden" name="orden" placeholder="25">
+        <input type="text" class="form-control" id="orden" name="orden" placeholder="25" required>
       </div>
     </div>
     @if (count($proveedores) > 0)
       <div class="form-group">
          <label class="col-sm-2 control-label">Proveedor</label>
          <div class="col-sm-10 selectContainer">
-             <select class="form-control" name="proveedor">
+             <select class="form-control" name="proveedor" required>
 
                  @foreach ($proveedores as $proveedor )
                    <option value="{{ $proveedor->id }}">
